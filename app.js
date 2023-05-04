5595c0ab3713d87b5e598944453f4efee90050e1
Eneas Junior <eneas.silva.jr@gmail.com>
2023-05-04 11:36:31 -0300

    Finished adding Todos. Start doing todos deletion.

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ formAddTodo.addEventListener('submit', event => {
     const inputValue = event.target.add.value.trim()
 
     if (inputValue.length) {
-        containerTodos.innerHTML = `
+        containerTodos.innerHTML += `
         <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${inputValue}</span>
         <i class="far fa-trash-alt delete"></i>
       </li>
         `
     }
+    formAddTodo.reset()
+})
+
+containerTodos.addEventListener('click', event => {
+    const clickedElement = event.target.classList
+    console.log(clickedElement)
 })
