879009459fde489b36f233a13efcf4836927178d
Eneas Junior <eneas.silva.jr@gmail.com>
2023-05-04 11:11:25 -0300

    Starting todo list project. Withe files, form and list references

---
+++ app.js
@@ -0,0 +1,17 @@
+const formAddTodo = document.querySelector('.form-add-todo')
+const containerTodos = document.querySelector('.todos-container')
+
+formAddTodo.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const inputValue = event.target.add.value.trim()
+
+    if (inputValue.length) {
+        containerTodos.innerHTML = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>
+        `
+    }
+})
